793f1a0756047345f14added4f7a2b1aa6f43f4a
HectorFranc0u <halejandrofb@gmail.com>
2023-02-26 17:24:09 -0600

    Primer Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Parcial I - Programacion IV</h1>
+  <br>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld/>
+  <p>Hector Alejandro Franco Berrios</p>
+  <p>Geovanny Mauricio Alvarez</p>
+  <p>David Arnoldo Hernandez Gomez</p>
+  <p>Rene Ismael Fuentes Benitez</p>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
